6cae43a588072862042ea9d5440f852758a2507f
Hikmet Sadigzade <43891893+sadigzade@users.noreply.github.com>
2020-08-23 14:10:12 +0300

    Проделал адаптив для блоков: about, gallery, а также для nexttour. Некоторые общие стили определенных блоков разметил в глобальный файл, в global.scss.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
+const anchors = document.querySelectorAll('a.home-menu__link')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
+
 $(document).ready(function () {
   $('.gallery-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 3000
+    dots: true
+    //autoplay: true,
+    //autoplaySpeed: 3000
   });
 });
